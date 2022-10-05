c6ed48d0930480a939b44133643b40480701c11f
Julka <Juliannaknop@gmail.com>
2022-10-05 20:26:13 +0200

    lekka zmiana planu plus podstawowe, wane rzeczy

--- jaka pogoda.py
+++ jaka pogoda.py
@@ -1,16 +1,22 @@
 #potrzebne rzeczy
-
-#przywitanie w aplikacji
+import requests
 
 #zdefiniowanie funkcji, która przedstawi informacje, o które może zapytać użytkownik
-#pogoda na dziś, pogoda na najbliższy tydzień, kiedy najbliższy deszcz, pogoda na dany dzień
-#informacje, które program musi przedstawić: temperatura w ciągu dnia i nocy,
-#czy opady, jeśli tak - w jakich godzinach, prędkość wiatru
+#pogoda na dziś, pogoda na najbliższy tydzień, XD (to lepiej ograniczyć, bo umre)
 
 #ogarnięcie dostępu do danych pogodowych (czy w tym miejscu? wyjdzie w praniu)
 
+#przywitanie w aplikacji
+print("Witaj w Personalnej Pogodynce. Możesz tutaj sprawdzić aktualną pogodę i prognozę na najbliższe dni.")
+
+while True:
+
 #input użytkownika
+
+#input uzytkownika - lokalizacja
 
 #wywołanie funkcji dotyczącej poszukiwanego info
 
 #input użytkownika + czy zamknąć program
+    czy_wyjść = input("Czy zakończyć działanie programu? T/N ")
+    if czy_wyjść == "T": break
