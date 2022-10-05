b911908c6d78b146b85b8e01afba421c7737884b
Julka <Juliannaknop@gmail.com>
2022-10-05 18:39:47 +0200

    plan działania

---
+++ jaka pogoda.py
@@ -0,0 +1,16 @@
+#potrzebne rzeczy
+
+#przywitanie w aplikacji
+
+#zdefiniowanie funkcji, która przedstawi informacje, o które może zapytać użytkownik
+#pogoda na dziś, pogoda na najbliższy tydzień, kiedy najbliższy deszcz, pogoda na dany dzień
+#informacje, które program musi przedstawić: temperatura w ciągu dnia i nocy,
+#czy opady, jeśli tak - w jakich godzinach, prędkość wiatru
+
+#ogarnięcie dostępu do danych pogodowych (czy w tym miejscu? wyjdzie w praniu)
+
+#input użytkownika
+
+#wywołanie funkcji dotyczącej poszukiwanego info
+
+#input użytkownika + czy zamknąć program
