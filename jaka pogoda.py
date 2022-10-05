f2311e81ca559103b9f5eda1b13d213f1f6d61fc
Julka <Juliannaknop@gmail.com>
2022-10-05 21:14:15 +0200

    dodany link do pogody i wprowadzona moliwosc podania miasta

--- jaka pogoda.py
+++ jaka pogoda.py
@@ -1,6 +1,9 @@
 #potrzebne rzeczy
 import requests
 
+#link do pogody
+link = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=9bf42cfd7b0efdc08e1d18087d83f5fe"
+
 #zdefiniowanie funkcji, która przedstawi informacje, o które może zapytać użytkownik
 #pogoda na dziś, pogoda na najbliższy tydzień, XD (to lepiej ograniczyć, bo umre)
 
@@ -10,13 +13,12 @@ import requests
 print("Witaj w Personalnej Pogodynce. Możesz tutaj sprawdzić aktualną pogodę i prognozę na najbliższe dni.")
 
 while True:
-
-#input użytkownika
-
-#input uzytkownika - lokalizacja
-
+    #dla jakiego miasta będzie sprawdzana pogoda
+    lokalizacja = input("Podaj nazwę miasta, w którym chcesz sprawdzić pogodę: ")
+    x = link.format(lokalizacja)
+ 
 #wywołanie funkcji dotyczącej poszukiwanego info
 
 #input użytkownika + czy zamknąć program
-    czy_wyjść = input("Czy zakończyć działanie programu? T/N ")
-    if czy_wyjść == "T": break
+    czy_wyjść = input("Czy zakończyć działanie programu? t/n ")
+    if czy_wyjść == "t": break
