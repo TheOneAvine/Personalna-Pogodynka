cc75bb3b8e66a436238ea41e0262a0b9f2990d7d
Julka <Juliannaknop@gmail.com>
2022-10-10 13:50:25 +0200

    lekkie porzdkowanie

--- jaka pogoda.py
+++ jaka pogoda.py
@@ -2,27 +2,26 @@ import requests
 
 #linki do pogody
 link_dzisiaj = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=9bf42cfd7b0efdc08e1d18087d83f5fe&lang=pl"
-link_5dni = "api.openweathermap.org/data/2.5/forecast?q={}&appid=9bf42cfd7b0efdc08e1d18087d83f5fe&lang=pl"
+link_5dni = "https://api.openweathermap.org/data/2.5/forecast?q={}&appid=9bf42cfd7b0efdc08e1d18087d83f5fe&lang=pl"
 
 
 #fuknkcje drukujące pożądane info
 def dzisiaj():
     temp = pogoda_dzisiaj["main"]["temp_max"] - 273.15
-    print("W tym momencie w miejscowości", lokalizacja, "panuje temperatura:", temp, "stopni.")
+    print("W tym momencie w miejscowości", lokalizacja, "panuje temperatura:", int(temp), "stopni.")
     temp_odczuwalna = pogoda_dzisiaj["main"]["feels_like"] - 273.15
-    print("Temperatura odczuwalna wynosi", temp_odczuwalna, "stopni.")
+    print("Temperatura odczuwalna wynosi", int(temp_odczuwalna), "stopni.")
     warunki = pogoda_dzisiaj["weather"][0]["description"]
     print("Na dworze jest", warunki +".")
     ciśnienie = pogoda_dzisiaj["main"]["pressure"]
     print("Ciśnienie wynosi", ciśnienie, "hPa.")
     wilgotność = pogoda_dzisiaj["main"]["humidity"]
-    print("Wilgotność jest na poziomie", wilgotność + "%")
+    print("Wilgotność jest na poziomie", wilgotność, "%")
     wiatr = pogoda_dzisiaj["wind"]["speed"]
-    print("Wiatr ma prędkość", wiatr + "m/s.")
+    print("Wiatr ma prędkość", wiatr, "m/s.")
     
     
 def jutro():
-    #pogoda na jutro
     pass
 
 def tydzien():
@@ -36,10 +35,15 @@ print("Witaj w Personalnej Pogodynce. Możesz tutaj sprawdzić aktualną pogodę
 while True:
     #dla jakiego miasta będzie sprawdzana pogoda
     lokalizacja = input("Podaj nazwę miasta, w którym chcesz sprawdzić pogodę: ")
+
+    #pobieramy dane
     x = link_dzisiaj.format(lokalizacja)
     pogoda_dzisiaj = requests.get(x).json()
+    y = link_5dni.format(lokalizacja)
+    pogoda = requests.get(y).json()
     
     dzisiaj()
+
 #wywołanie funkcji dotyczącej poszukiwanego info
 
 #input użytkownika + czy zamknąć program
